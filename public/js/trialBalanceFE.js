f517984e1e036b24222344239c327799558315a6
ktmangezi <95950915+ktmangezi@users.noreply.github.com>
2025-03-29 13:15:34 +0200

    Update trialBalanceFE.js

--- public/js/trialBalanceFE.js
+++ public/js/trialBalanceFE.js
@@ -167,10 +167,15 @@ fetch('/currencies')
 
                                     const openingCashBalance = calculateCashBalance()
 
-                                    let theStartDate = moment(startDate);//convert to the format dd/mm/yy using moment
-                                    let theEndDate = moment(endDate);//convert to the format dd/mm/yy using moment
-                                    theStartDate = theStartDate.format('DD/MM/YYYY');
-                                    theEndDate = theEndDate.format('DD/MM/YYYY');
+
+                                    const momntStartDate1 = moment.tz(startDate, "Africa/Harare").startOf('day'); // Midnight in Zimbabwe
+                                    const momntEndDate1 = moment.tz(endDate, "Africa/Harare").endOf('day'); // end of day  in Zimbabwe
+
+                                    // Convert back to JavaScript Date objects (optional, only if needed)
+                                    startDate = momntStartDate1.toDate();
+                                    endDate = momntEndDate1.toDate();
+                                    const theStartDate = startDate.format('DD/MM/YYYY');
+                                    const theEndDate = endDate.format('DD/MM/YYYY');
                                     //create another span
                                     const trialBalanceSpan = document.querySelector('.dateRange')
 
@@ -593,8 +598,9 @@ fetch('/currencies')
                                                 const parts = date.split("/");
                                                 const formattedDate = parts[1] + "/" + parts[0] + "/" + parts[2];
                                                 const formattedDates2 = new Date(formattedDate);
+                                                const formattedDates2Zim = moment.tz(formattedDates2, "Africa/Harare").startOf('day').toDate();
 
-                                                if (formattedDates2.getTime() <= endDate.getTime()) {//CODE FOR 
+                                                if (formattedDates2Zim.getTime() <= endDate.getTime()) {//CODE FOR 
                                                     //TIRIMO CHECK THE TOTALS OF THE AVAILABLE CATEGORIES
                                                     if (each.category === data.CashFlowCategory && data.CashFlowType === 'Payout') {
                                                         totalPayOutCatAmnt1 = parseFloat(totalPayOutCatAmnt1) + parseFloat(data.CashFlowCashEquiv);
